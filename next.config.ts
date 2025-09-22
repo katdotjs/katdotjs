a38f99e0ce26572353464d5ce6bf2f3e2faa068f
Kat Graham <kat.exe11@yahoo.com>
2025-09-21 23:13:09 -0400

    8

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   basePath: "/katdotjs",
   output: "export",
   reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
